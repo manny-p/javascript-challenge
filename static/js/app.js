b1c407d48d47637297f5450a1172f26d2973a308
Manny <parra.manny@gmail.com>
2021-08-19 14:46:35 -0600

    add event listener for filtered data

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,12 @@
 // from data.js
-let tableData = data;
+let tableData = data
 
 // select tbody element
 let tbody = d3.select('tbody')
 
 // build table
 const populateTable = (dataInput) => {
-    tbody.innerHTML = ''
+    tbody.innerHTML = ``
 
     dataInput.forEach(sighting => {
         let row = tbody.append('tr')
@@ -22,3 +22,36 @@ const populateTable = (dataInput) => {
 
 // load data
 populateTable(tableData)
+
+const handleQuery = () => {
+    d3.event.preventDefault()
+    tbody.html('')
+
+    let dateQuery = d3.select('#datetime').property('value')
+
+// filter results
+    let filter = tableData.filter(tableData => tableData.datetime === dateQuery)
+    filter.forEach(dateSelect => {
+        let row = tbody.append('tr')
+        Object.entries(dateSelect).forEach(function ([key, value]) {
+            let cell = row.append('td')
+            cell.text(value)
+        })
+    })
+}
+
+d3.select('#filter-btn').on('click', handleQuery)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
